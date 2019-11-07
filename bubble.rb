00b43232901de8ed1d1697a593f22982d0bc9289
chucksef <chucksef@gmail.com>
2019-11-07 12:08:38 -0700

    Refactored Code

--- bubble.rb
+++ bubble.rb
@@ -1,4 +1,4 @@
-to_sort = [108,4,15,42,8,23,16]
+array_to_sort = [108,4,15,42,8,23,16]
 
 def bubble_sort(arr)
     needs_checking = true
@@ -6,14 +6,10 @@ def bubble_sort(arr)
         needs_checking = false
         i = 0
         while i < arr.length-1
-            puts "Checking if #{arr[i]} > #{arr[i+1]}"
             if arr[i] > arr[i+1]
-                val1 = arr[i]
-                val2 = arr[i+1]
-                puts "It is! Switching them!"
-                arr[i] = val2
-                arr[i+1] = val1
-                puts "Re-ordered array: #{arr.to_s}"
+                val = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = val
                 needs_checking = true
             end
             i += 1
@@ -22,4 +18,4 @@ def bubble_sort(arr)
     arr
 end
 
-puts bubble_sort(to_sort)
+puts bubble_sort(array_to_sort)
